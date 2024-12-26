4a5b4a94c632a6d28c54137682247fd8a8407159
songwonjoon <djckoreas@naver.com>
2024-12-26 11:19:04 +0900

    add Database module file

---
+++ Database.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import mysql.connector
+
+class Database:
+    def __init__(self, host, port, user, password, database=None):
+        self.connection = mysql.connector.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.connection.cursor()
+
+    def createTable(self, table_name, columns):
+        columns_definition = ", ".join([f"{name} {datatype}" for name, datatype in columns.items()])
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_definition})")
+
+    def insertData(self, table_name, row_data):
+        placeholders = ", ".join(["%s"] * len(row_data))
+        columns = ", ".join(row_data.keys())
+        values = tuple(row_data.values())
+        query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+        self.cursor.execute(query, values)
+        self.connection.commit()
+        
+
+    def updateData(self, table_name, updates, condition):
+        """Update data in a table"""
+        set_clause = ", ".join([f"{col} = %s" for col in updates.keys()])
+        query = f"UPDATE {table_name} SET {set_clause} WHERE {condition}"
+        self.cursor.execute(query, tuple(updates.values()))
+        self.connection.commit()
+
+    def selectData(self, table_name, columns="*", condition=None):
+        query = f"SELECT {columns} FROM {table_name}"
+        if condition:
+            query += f" WHERE {condition}"
+        self.cursor.execute(query)
+        results = self.cursor.fetchall()
+        return results
+
+    def closeConnection(self):
+        self.cursor.close()
+        self.connection.close()
